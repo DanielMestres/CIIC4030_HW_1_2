a4bc20dbca81da6687a70eb2537e65fe9b70d39f
DanielMestres <daniel.mestres@upr.edu>
2022-02-13 23:04:51 -0400

    finishing touches

--- lexer.py
+++ lexer.py
@@ -2,7 +2,7 @@
 # Daniel Mestres Pinero_802-15-4744
 # CIIC4030-036
 # Assignment_1_Scanner
-# Run:
+# Run: (Linux)
 #   python3 lexer.py "input file name"
 # References:
 #   https://www.dabeaz.com/ply/ply.html
@@ -12,44 +12,43 @@ from ply import lex as lex
 import sys
 
 # List of reserved words
-keywords = {
-    'if'    : 'IF',     'then'  : 'THEN',
-    'else'  : 'ELSE',   'map'   : 'MAP',
-    'to'    : 'TO',     'let'   : 'LET',
-    'in'    : 'IN',     'null'  : 'NULL',
-    'true'  : 'BOOL',   'false' : 'BOOL',
+words = {
+    'if'    : 'KEYWORD',    'then'  : 'KEYWORD',
+    'else'  : 'KEYWORD',    'map'   : 'KEYWORD',
+    'to'    : 'KEYWORD',    'let'   : 'KEYWORD',
+    'in'    : 'KEYWORD',    'null'  : 'NULL',
+    'true'  : 'BOOL',       'false' : 'BOOL',
 
     'number?'   : 'PRIM', 'function?' : 'PRIM',
     'list?'     : 'PRIM', 'null?'     : 'PRIM',
     'cons?'     : 'PRIM', 'cons'      : 'PRIM',
-    'first'     : 'PRIM', 'rest'      : 'PRIM',
+    'first'     : 'PRIM', 'rest'      : 'PRIM'
 }
 
 # List of reserved symbols
 symbols = {
-    '(' : 'LPAREN',     ')' : 'RPAREN',
-    '[' : 'LBRACKET',   ']' : 'RBRACKET',
-    ',' : 'COMMA',      ';' : 'SEMICOLON',
-    '+' : 'SIGN',       '-' : 'SIGN',
-    '~' : 'UNOP',       '*' : 'BINOP',
-    '/' : 'BINOP',      '=' : 'BINOP',
-    '!=' : 'BINOP',     '<' : 'BINOP',
-    '>' : 'BINOP',      '<=' : 'BINOP',
-    '>=' : 'BINOP',     '&' : 'BINOP',
-    '|' : 'BINOP',      ':=' : 'BINOP'
+    '(' : 'DELIMITER',      ')' : 'DELIMITER',
+    '[' : 'DELIMITER',      ']' : 'DELIMITER',
+    ',' : 'DELIMITER',      ';' : 'DELIMITER',
+
+    '+'     : 'SIGN',       '-' : 'SIGN',
+    '~'     : 'UNOP',       '*' : 'BINOP',
+    '/'     : 'BINOP',      '=' : 'BINOP',
+    '!='    : 'BINOP',      '<' : 'BINOP',
+    '>'     : 'BINOP',      '<=' : 'BINOP',
+    '>='    : 'BINOP',      '&' : 'BINOP',
+    '|'     : 'BINOP',      ':=' : 'BINOP'
 }
 
 # Token names
 tokens = [
     'INT',
+
     'ID',
+    'KEYWORD',
+
     'SYMBOL',
-    'LPAREN',
-    'RPAREN',
-    'LBRACKET',
-    'RBRACKET',
-    'COMMA',
-    'SEMICOLON',
+    'DELIMITER',
     'NULL',
     'BOOL',
     'UNOP',
@@ -58,6 +57,7 @@ tokens = [
     'PRIM'
 ]
 
+# regex rules
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
@@ -70,28 +70,34 @@ def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
-# Regular expression rules
 t_ignore  = ' \t'
+
+# Digit+
 t_INT = r'\d+'
 
+# AlphaOther {AlphaOtherNumeric}*
 def t_ID(t):
     r'[a-zA-Z_?][a-zA-Z0-9_?]*'
-    t.type = keywords.get(t.value, 'ID')
+    # Checks for reserved words
+    t.type = words.get(t.value, 'ID')
     return t
 
+# {Delimiter | Operator} {Delimiter | Operator}*
 def t_SYMBOL(t):
     r'[^a-zA-Z0-9_?_\n][^a-zA-Z0-9_?_;_\n_\s]*'
+    # Checks for reserved symbols
     t.type = symbols.get(t.value, 'SYMBOL')
+    # Returns an error if symbol is not recognized
     if(t.type == 'SYMBOL'):
         t_error(t)
     else:
         return t
 
+# Read input
+data = open(sys.argv[1])
+
 # Build lexer
 lexer = lex.lex()
-
-# Read INPUT FILE
-data = open(sys.argv[1])
 
 # Tokenize
 with data as fp:
